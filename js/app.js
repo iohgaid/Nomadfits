b5032279afc61c4a3c8d5e7dfba14e561c77e030
mariam - hassan <maryamahassan6020@gmail.com>
2026-07-10 18:19:43 -0700

    dark mode

--- js/app.js
+++ js/app.js
@@ -603,3 +603,19 @@ contactForm.addEventListener("submit", (e) => {
 
 renderCart();
 fetchProducts();
+const themeToggle = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+}
+
+themeToggle?.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark-mode")
+      ? "dark"
+      : "light"
+  );
+});
